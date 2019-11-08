8805e3b6b157877039ac4593d31cab23a9f02701
Aarti Jivrajani <aartij17@gmail.com>
2019-11-08 12:58:36 -0800

    Created Invite model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_08_204700) do
+ActiveRecord::Schema.define(version: 2019_11_08_205639) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -30,6 +30,13 @@ ActiveRecord::Schema.define(version: 2019_11_08_204700) do
     t.datetime "end_time"
     t.string "title"
     t.string "description"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "invites", force: :cascade do |t|
+    t.string "message"
+    t.string "guest_email"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
