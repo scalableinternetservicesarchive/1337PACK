0c7c7cd39c85eb449f0cc7b6b796088d80af27ae
Aarti Jivrajani <aartij17@gmail.com>
2019-11-08 12:42:53 -0800

    Created User model

--- db/schema.rb
+++ db/schema.rb
@@ -10,74 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_05_173849) do
+ActiveRecord::Schema.define(version: 2019_11_08_203652) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "comments", force: :cascade do |t|
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.bigint "user_id"
-    t.bigint "event_id"
-    t.string "content"
-    t.index ["event_id"], name: "index_comments_on_event_id"
-    t.index ["user_id"], name: "index_comments_on_user_id"
-  end
-
-  create_table "events", force: :cascade do |t|
-    t.string "venue"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.bigint "comment_id"
-    t.bigint "user_id"
-    t.string "title"
-    t.datetime "start_time"
-    t.datetime "end_time"
-    t.string "description"
-    t.string "host"
-    t.index ["comment_id"], name: "index_events_on_comment_id"
-    t.index ["user_id"], name: "index_events_on_user_id"
-  end
-
-  create_table "invites", force: :cascade do |t|
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.bigint "event_id"
-    t.bigint "rsvp_id"
-    t.bigint "user_id"
-    t.string "message"
-    t.string "guest_email"
-    t.index ["event_id"], name: "index_invites_on_event_id"
-    t.index ["rsvp_id"], name: "index_invites_on_rsvp_id"
-    t.index ["user_id"], name: "index_invites_on_user_id"
-  end
-
-  create_table "rsvps", force: :cascade do |t|
-    t.boolean "answer"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.bigint "user_id"
-    t.bigint "event_id"
-    t.integer "num_guests"
-    t.index ["event_id"], name: "index_rsvps_on_event_id"
-    t.index ["user_id"], name: "index_rsvps_on_user_id"
-  end
-
-  create_table "tweets", force: :cascade do |t|
-    t.string "message", null: false
-    t.string "author", null: false
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-  end
-
   create_table "users", force: :cascade do |t|
-    t.string "username"
+    t.string "email"
     t.string "password"
+    t.string "first_name"
+    t.string "last_name"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.bigint "event_id"
-    t.index ["event_id"], name: "index_users_on_event_id"
   end
 
 end
