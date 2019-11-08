0f0880ae3197d28c5de555082ec4774183dcc7ad
Aarti Jivrajani <aartij17@gmail.com>
2019-11-08 13:03:01 -0800

    Created RSVP Model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_08_205639) do
+ActiveRecord::Schema.define(version: 2019_11_08_210036) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -37,6 +37,14 @@ ActiveRecord::Schema.define(version: 2019_11_08_205639) do
   create_table "invites", force: :cascade do |t|
     t.string "message"
     t.string "guest_email"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "rsvps", force: :cascade do |t|
+    t.string "response"
+    t.integer "num_guests"
+    t.string "guest_name"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
