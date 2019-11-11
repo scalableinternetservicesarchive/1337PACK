0c96a0fadc8f6da6f397a516f9f4b2373a03c894
Aarti Jivrajani <aartij17@gmail.com>
2019-11-11 15:24:48 -0800

    Added model validations for comment, event and invite

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,2 +1,4 @@
 class Event < ApplicationRecord
+    validates :end_time,
+              presence: false
 end
--- app/models/invite.rb
+++ app/models/invite.rb
@@ -1,2 +1,9 @@
 class Invite < ApplicationRecord
+    VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
+    validates :guest_email,
+              presence: true, length: { maximum: 255 },
+              format: { with: VALID_EMAIL_REGEX },
+              uniqueness: { case_sensitive: false }
+    validates :message,
+              length: { maximum: 1000}
 end
