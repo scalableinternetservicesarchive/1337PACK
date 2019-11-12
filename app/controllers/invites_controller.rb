0baf57760db039d475c451c8d5c71dea7134e3a7
Yuning Shen <ynshen23@gmail.com>
2019-11-11 21:42:32 -0800

    Correct model name: Invites to Invite

--- app/controllers/invites_controller.rb
+++ app/controllers/invites_controller.rb
@@ -1,12 +1,11 @@
 class InvitesController < ApplicationController
-    # do :set_comment function only just before show, edit ... actions
     before_action :set_invites, only: [:show, :edit, :update, :destroy]
     # allow following to diable authentification
     skip_before_action :verify_authenticity_token
 
     # POST /invites
     def create
-        @invite = Invites.new(invite_params)
+        @invite = Invite.new(invite_params)
         if @invite.save
             render json: @invite, status: :created
         else
@@ -16,12 +15,12 @@ class InvitesController < ApplicationController
 
     # GET /invites
     def index
-        render json: Invites.all
+        render json: Invite.all
     end
 
     # GET /invites/{id}
     def show
-        render json: Invites.find(invite_params[:id])
+        render json: Invite.find(invite_params[:id])
     end
 
     # PUT/Patch /invites/{id}
@@ -33,10 +32,9 @@ class InvitesController < ApplicationController
         end
     end
 
-    # DELETE /comment/{id}
+    # DELETE /invites/{id}
     def destroy
         if @invite.destroy
-            nvite.where(parent_id: commen
             head :no_content
         else
             render json: @invite.errors, status: :unprocessable_entity
@@ -45,11 +43,11 @@ class InvitesController < ApplicationController
 
     private
         def set_invite
-            @invite = Invites.find(invite_params[:id])
+            @invite = Invite.find(invite_params[:id])
         end
 
         def invite_params
-            # params needed for create a comment
+            # params needed for create a invite
             params.permit(:id, :event_id, :guest_email, :message)
         end
 end
