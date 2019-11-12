bc277c3806436f4c872dd1d7adc43dc29a69b40e
Yuning Shen <ynshen23@gmail.com>
2019-11-11 23:51:17 -0500

    copy invite model from controller and set event id to be required

--- app/models/invite.rb
+++ app/models/invite.rb
@@ -1,10 +1,11 @@
 class Invite < ApplicationRecord
-    validates :message,
-              length: { maximum: 250}
-    # must associate with an event
-    validates :event_id,
-              presence: true
-    # must have a target email
+    VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
     validates :guest_email,
+              presence: true, length: { maximum: 255 },
+              format: { with: VALID_EMAIL_REGEX },
+              uniqueness: { case_sensitive: false }
+    validates :message,
+              length: { maximum: 1000}
+    validates :event_id,
               presence: true
 end
