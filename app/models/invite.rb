b8baa04f44ba2b5693cfb4a271a8d052098e3bd7
Yuning Shen <ynshen23@gmail.com>
2019-11-11 23:48:38 -0500

    add invite model

--- app/models/invite.rb
+++ app/models/invite.rb
@@ -1,2 +1,10 @@
 class Invite < ApplicationRecord
+    validates :message,
+              length: { maximum: 250}
+    # must associate with an event
+    validates :event_id,
+              presence: true
+    # must have a target email
+    validates :guest_email,
+              presence: true
 end
