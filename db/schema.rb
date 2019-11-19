f5ffcb256083cb7d458183d67ec5cb5ff56ae610
Aarti Jivrajani <aartij17@gmail.com>
2019-11-19 13:59:54 -0800

    Added user id reference to the invire table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_18_035135) do
+ActiveRecord::Schema.define(version: 2019_11_19_215630) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -48,7 +48,9 @@ ActiveRecord::Schema.define(version: 2019_11_18_035135) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.bigint "event_id"
+    t.bigint "user_id"
     t.index ["event_id"], name: "index_invites_on_event_id"
+    t.index ["user_id"], name: "index_invites_on_user_id"
   end
 
   create_table "rsvps", force: :cascade do |t|
