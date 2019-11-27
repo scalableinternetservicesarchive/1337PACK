bd8a5553c68e77e3df8fbba30200ff2e372c77b4
Yijun Xiao <yijunxiao@cs.ucsb.edu>
2019-11-27 15:29:30 -0800

    Event belongs_to User set to optional

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,5 +1,5 @@
 class Event < ApplicationRecord
-    belongs_to :user
+    belongs_to :user, optional: true
     has_many :comments, dependent: :destroy
     has_many :invites, dependent: :destroy
     has_many :rsvps, dependent: :destroy
