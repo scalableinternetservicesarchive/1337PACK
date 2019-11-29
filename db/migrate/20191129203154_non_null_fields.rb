80236a9d1926bc51338b708b4e86e17069d377d6
Aarti Jivrajani <aartij17@gmail.com>
2019-11-29 13:00:29 -0800

    Added non-null constraint for testing purposes

---
+++ db/migrate/20191129203154_non_null_fields.rb
@@ -0,0 +1,5 @@
+class NonNullFields < ActiveRecord::Migration[6.0]
+  def change
+    change_column_null :events, :title, false
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_26_055500) do
+ActiveRecord::Schema.define(version: 2019_11_29_203154) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -33,7 +33,7 @@ ActiveRecord::Schema.define(version: 2019_11_26_055500) do
     t.string "street_address"
     t.datetime "start_time"
     t.datetime "end_time"
-    t.string "title"
+    t.string "title", null: false
     t.string "description"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
