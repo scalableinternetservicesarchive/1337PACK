99338bce274688f8b4ac9a00fdcf67bdf73b9be0
Yuning Shen <ynshen23@gmail.com>
2019-12-03 00:30:25 -0800

    update seeds.rb to 10,000 records for user and events

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,26 +2,26 @@
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
 
-for i in 1..800
+for i in 1..10000
     p "#{i} Aloha! :)"
     @user = User.create!(
+            id: i,
             email: "#{i}-hi.pack@g.com",
             password: "test_password",
             password_confirmation: "test_password",
             first_name: "hi.pack",
             last_name: "hi.lastname"
     )
-    for e in 1..3
-        @event = Event.create!(
-            user_id: @user.id,
-            host_name: @user.email,
-            location_name: "blr",
-            street_address: "blah",
-            start_time: "2019-11-30 01:52:00.67957",
-            end_time: "2019-11-30 01:52:00.67957",
-            title: "foo",
-            description: @user.email
-        )
+    @event = Event.create!(
+        id: i,
+        user_id: @user.id,
+        host_name: @user.email,
+        location_name: "blr",
+        street_address: "blah",
+        start_time: "2019-11-30 01:52:00.67957",
+        end_time: "2019-11-30 01:52:00.67957",
+        title: "foo",
+        description: @user.email
+    )
     end
 end
-
